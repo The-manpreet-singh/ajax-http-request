65bd1bdda379f167cbd15d9d060dd91ad7df2f91
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-04 14:25:42 +0530

    transform data using slice method

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -15,7 +15,14 @@ class Blog extends Component {
     componentDidMount () {
           axios.get('https://jsonplaceholder.typicode.com/posts')
              .then( response => {
-                 this.setState({posts: response.data});
+                 const posts= response.data.slice(0,4);
+                 const updatedPosts= posts.map(post =>{
+                     return {
+                         ...post,
+                         author: 'Max'
+                     }
+                 })
+                 this.setState({posts: updatedPosts});
                   //console.log(response);
              })
     };
@@ -23,7 +30,7 @@ class Blog extends Component {
     render () {
 
          const posts= this.state.posts.map(post => {
-             return  <Post key={post.id} title={post.title}/>
+             return  <Post key={post.id} title={post.title} author={post.author}/>
          });
 
         return (
