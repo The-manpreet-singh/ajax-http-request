3fcfa3c9c19961f4e4d77dc8bdd285a4de7a9591
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-04 14:09:05 +0530

    display fetch data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,20 +8,28 @@ import './Blog.css';
 
 class Blog extends Component {
 
+    state={
+        posts: []
+    }
+
     componentDidMount () {
           axios.get('https://jsonplaceholder.typicode.com/posts')
              .then( response => {
-                  console.log(response);
+                 this.setState({posts: response.data});
+                  //console.log(response);
              })
     };
 
     render () {
+
+         const posts= this.state.posts.map(post => {
+             return  <Post key={post.id} title={post.title}/>
+         });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                   {posts}
                 </section>
                 <section>
                     <FullPost />
