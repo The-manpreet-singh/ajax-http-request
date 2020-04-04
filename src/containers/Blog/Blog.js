2cd41283da7ec77ce85a9b936a20938049ea8612
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-04 11:55:24 +0530

    install axios to get the data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,6 +7,14 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+    componentDidMount () {
+          axios.get('https://jsonplaceholder.typicode.com/posts')
+             .then( response => {
+                  console.log(response);
+             })
+    };
+
     render () {
         return (
             <div>
