3ef5233a5c9f5ecc9a1795e6d6f404fdf23b4523
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-05 13:55:03 +0530

    add catch method

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -26,6 +26,10 @@ class Blog extends Component {
                  this.setState({posts: updatedPosts});
                   //console.log(response);
              })
+             .catch( error => {
+                 //console.log(error);
+                 this.setState({error: true});
+             })
     };
 
     postSelectedHandler = (id) => {
@@ -33,15 +37,18 @@ class Blog extends Component {
     }
 
     render () {
-
-         const posts= this.state.posts.map(post => {
-             return  <Post 
-                   key={post.id} 
-                   title={post.title} 
-                   author={post.author}
-                   clicked={()=> this.postSelectedHandler(post.id)} />
-         });
-
+         let posts = <p style={{textAlign:"center"}}>Something went wrong</p>
+         if(!this.state.error){
+            posts= this.state.posts.map(post => {
+                return  <Post 
+                      key={post.id} 
+                      title={post.title} 
+                      author={post.author}
+                      clicked={()=> this.postSelectedHandler(post.id)} />
+            });
+   
+         }
+        
         return (
             <div>
                 <section className="Posts">
