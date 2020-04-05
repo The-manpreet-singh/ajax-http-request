0cc2c7ff1908ffd4a551890e952aa216fd64526d
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-05 12:03:25 +0530

    update the data using life cycle

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
+    state= {
+        loadedPost :null
+    }
+     
+    componentDidUpdate () {
+        if(this.props.id){
+            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)){
+                axios.get('https://jsonplaceholder.typicode.com/posts/' +  this.props.id)
+                .then(response => {
+                    //console.log(response);
+                    this.setState({loadedPost: response.data});
+                })
+            }
+            
+        }
+       
+    }
+
     render () {
         let post = <p style={{textAlign:"center"}}>Please select a Post!</p>;
         if(this.props.id){
+            post = <p style={{textAlign:"center"}}>Loading...!</p>;
+        }
+       
+        if(this.state.loadedPost){
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.content}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
