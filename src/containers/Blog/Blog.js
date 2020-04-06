e7dd21f303ac87d03805d7611bd57ed1878e1b0f
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-06 10:17:26 +0530

    add interceptors

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -16,6 +16,7 @@ class Blog extends Component {
     componentDidMount () {
           axios.get('https://jsonplaceholder.typicode.com/posts')
              .then( response => {
+                 
                  const posts= response.data.slice(0,4);
                  const updatedPosts= posts.map(post =>{
                      return {
