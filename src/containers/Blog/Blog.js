0d4f743ac74e55841619480d23e13101a7fd0f0e
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-08 12:26:41 +0530

    add component route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 //import axios from '../../axios';
 import './Blog.css';
 import Posts from './Posts/Posts';
+import { Route } from 'react-router-dom';
 
 class Blog extends Component {
 
@@ -18,8 +19,10 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-              <Posts />  
-              
+             {/*  <Posts />  */} 
+             {/*  <Route path="/" exact render={() => <h1>Home</h1>} />
+              <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+              <Route path="/" exact component={Posts} />
             </div>
         );
     }
