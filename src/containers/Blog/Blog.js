5b8c75c4bd996e2ea88bc83995058b8862c9111e
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-08 12:36:37 +0530

    add new posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import { Route } from 'react-router-dom';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
 
@@ -23,6 +24,8 @@ class Blog extends Component {
              {/*  <Route path="/" exact render={() => <h1>Home</h1>} />
               <Route path="/" render={() => <h1>Home 2</h1>} /> */}
               <Route path="/" exact component={Posts} />
+              <Route path="/new-post" exact component={NewPost} />
+
             </div>
         );
     }
